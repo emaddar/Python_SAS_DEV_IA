a90828b8162b3f1961962e7a793c10245a22ee1c
emaddar <e.a.darwich@gmail.com>
2022-09-28 20:54:10 +0200

    j

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ page_names_to_funcs = {
     "Page 2": page2,
     "Page 3": page3,
 }
-
+st.markdown("Hello")
 #selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 #page_names_to_funcs[selected_page]()
