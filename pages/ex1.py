8d6edff95989541f5b84b8ad53ed5588a2b16ab0
Emad_HP_Home <e.a.darwich@gmail.com>
2022-10-03 22:27:37 +0200

    new

--- pages/ex1.py
+++ pages/ex1.py
@@ -4,37 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-names = ["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"]
-df = pd.read_fwf("https://raw.githubusercontent.com/emaddar/Python_SAS_DEV_IA/main/Data/housing.data", header=None, names = names)
-# data_set_1.plot(kind='scatter', x='Population', y='Profit')
-# plt.show()
 
-B = df.B.values
-meanB = np.mean(B)
-sdB = np.std(B)
-print(((B-meanB)/sdB)[0])
-
-# # Insert X0 Column
-# Xd = df.drop(columns=['MEDV'])
-# Xd.insert(0, 'X0', 1)
-# print(Xd.head())
+df = pd.read_csv("https://raw.githubusercontent.com/emaddar/Python_SAS_DEV_IA/main/Data/ex1data1.csv")
 
 
-# # numpy array format
-# X = Xd.values
-# y = df.MEDV.values
-# # sample size
-# m = len(df.index)
-# print(m)
-# # Initialize theta
-
-# # number of features
-# n = X.shape[1]
-# print(n)
-
-# theta = np.ones(n)
-# print(theta)
 
 
-# hypo = X@theta
-# # print(hypo)
