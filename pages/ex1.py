4fc2170bfa81555644bbc785de13599b1eb34964
Emad_ASUS <e.a.darwich@gmail.com>
2022-10-03 21:13:15 +0200

    func.

--- pages/ex1.py
+++ pages/ex1.py
@@ -26,7 +26,7 @@ init_theta = np.zeros((n,1))
 # print(init_theta)
 
 
-iterations = 10000
+iterations = 1000
 alpha = 0.00001
 
 print(f"X shape = {X.shape}")
@@ -36,39 +36,37 @@ print(f"Theta shape = {init_theta.shape}")
 
 
 # simpler vectorized method
-def fonction_cout(X, y, theta):
-    m = len(y)
-    h = X @ theta
-    difference = h - y
-    square = np.square(difference)
-    J = 1 / (2 * m) * np.sum(square) 
-    return J
+# def fonction_cout(X, y, theta):
+#     m = len(y)
+#     h = X @ theta
+#     difference = h - y
+#     square = np.square(difference)
+#     J = 1 / (2 * m) * np.sum(square) 
+#     return J
 # print(fonction_cout(X, y, init_theta))
 
+def fonction_cout(X, y, theta):
+    m = len(y)
+    J = (1/(2*m))*np.transpose((X@theta - y))@(X@theta - y)
+    return J
 
 
-
-# Vectorized gradient descent method
 def gradientDescent(X, y, theta, alpha, iterations):
-    new_theta = theta.copy()
-    costs = []
-    for it in range(iterations):
-        sum = np.zeros(new_theta.shape)
-        m = len(X)
-        for i in range(m):
-            sum += (new_theta.T @ np.array([X[i]]).T - y[i]).item() * np.array([X[i]]).T
-        new_theta -= alpha * 1 / (2 * m) * sum
-        new_cost = fonction_cout(X, y, new_theta)
-        costs.append(new_cost)
-        deff = costs[it] - costs[it - 1]
-        print(f"Iteration :{it} , cout : {'{:.20f}'.format(deff)}")
-    return new_theta, costs
+    cout_list = []
+    for i in range(iterations):
+    
+        theta = theta - alpha*(1/m)*np.transpose(X)@(X@theta - y)
+            
+        cout_val = fonction_cout(X, y, theta)
+        cout_list.append(cout_val)
+
+    return theta, cout_list
 
 learned_theta, costs = gradientDescent(X, y, init_theta, alpha, iterations)
 
 
-print(learned_theta)
-print(costs[-5 : -1])
+# print(learned_theta)
+# print(costs[-5 : -1])
 # df = pd.DataFrame(costs, columns =['cout'])
 # df['iteration'] = df.index
 # df.plot(kind='scatter', x = "iteration", y = "cout")
